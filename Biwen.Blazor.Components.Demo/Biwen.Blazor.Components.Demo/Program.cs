679f720840502e87a18d6859d5c6d3277c3d444a
万雅虎 <vipwan@outlook.com>
2023-12-01 17:14:17 +0800

    init

---
+++ Biwen.Blazor.Components.Demo/Biwen.Blazor.Components.Demo/Program.cs
@@ -0,0 +1,34 @@
+using Biwen.Blazor.Components.Demo.Client.Pages;
+using Biwen.Blazor.Components.Demo.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddFluentUIComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
+
+app.Run();
---
+++ Biwen.Blazor.Components/wwwroot/modules.js
@@ -0,0 +1,17 @@
+﻿import "./prism.js";
+
+//加载css文件
+export function loadStyle(resourceUrl) {
+    return new Promise((resolve, reject) => {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.onload = () => resolve();
+        link.onerror = () => reject();
+        link.href = resourceUrl;
+        document.head.appendChild(link);
+    });
+}
+
+export function highlight(ele, flag, o) {
+    Prism.highlightAllUnder(ele, flag, o)
+}
